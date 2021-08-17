c24a163470ece2fb3ba912d9738269df052135f8
leorodrigues10 <leonelmiranda10@outlook.com>
2021-08-16 23:36:23 -0100

    add update method to post controller

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -13,7 +14,7 @@ class PostController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -27,8 +28,8 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -54,6 +55,7 @@ class PostController extends Controller
             'description' => $fields['description'],
             'slug' => Str::slug($fields['title'], '-'),
             'image_url' => $file_url,
+            'user_id' => 1
         ]);
 
         return response($post, 201);
@@ -63,7 +65,7 @@ class PostController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
@@ -79,20 +81,41 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $post = Post::find($id);
+
+        if(!$post) {
+            return response(['message' => 'Post not found']);
+        }
+
+        if($request->hasFile('file')) {
+
+            $file = $request->file('file');
+
+            $file_path = $file->store('public/postFiles');
+            $file_storage_url = Storage::url($file_path);
+            $file_url = url($file_storage_url);
+            $request->file = $file_url;
+        }
+
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->image_url = $request->file;
+        $post->save();
+
+        return response($post);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
